5ba68c43354eee0215f9d3f8b3cecec4661b235e
Gary Siladi <gergely.siladi@globallogic.com>
2016-11-22 23:38:10 +0100

    table widths

--- app/components/main.js
+++ app/components/main.js
@@ -56,13 +56,13 @@ class Main extends React.Component {
         <BootstrapTable exportCSV search data={this.state.data} options={this.options} striped={true} hover={true}>
           <TableHeaderColumn dataField="id" isKey={true} hidden>ID</TableHeaderColumn>
           <TableHeaderColumn dataField="datemilli" dataSort={true} hidden>datemilli</TableHeaderColumn>
-          <TableHeaderColumn dataField="name" width="150" dataSort={true}>Name</TableHeaderColumn>
-          <TableHeaderColumn dataField="email" width="150" dataSort={true}>Email</TableHeaderColumn>
-          <TableHeaderColumn dataField="class" width="150" dataSort={true}>Class</TableHeaderColumn>
-          <TableHeaderColumn dataField="title" width="150" dataSort={true}>Title</TableHeaderColumn>
+          <TableHeaderColumn dataField="name" width="100" dataSort={true}>Name</TableHeaderColumn>
+          <TableHeaderColumn dataField="email" width="100" dataSort={true}>Email</TableHeaderColumn>
+          <TableHeaderColumn dataField="class" width="100" dataSort={true}>Class</TableHeaderColumn>
+          <TableHeaderColumn dataField="title" width="100" dataSort={true}>Title</TableHeaderColumn>
           <TableHeaderColumn dataField="date" width="100" dataSort={true}>Date</TableHeaderColumn>
           <TableHeaderColumn dataField="grade" width="50" dataSort={true} dataAlign='center'>Mark</TableHeaderColumn>
-          <TableHeaderColumn dataField="percentage" width="150" dataFormat={this.percFormatter} dataSort={true}>%</TableHeaderColumn>
+          <TableHeaderColumn dataField="percentage" width="100" dataFormat={this.percFormatter} dataSort={true}>%</TableHeaderColumn>
           <TableHeaderColumn dataField="score" width="50" dataSort={true} dataAlign='center'>Score</TableHeaderColumn>
           <TableHeaderColumn dataField="maxscore" width="50" dataSort={true} dataAlign='center'>Max</TableHeaderColumn>
           <TableHeaderColumn dataField="time" width="100" dataSort={true}>Time</TableHeaderColumn>
